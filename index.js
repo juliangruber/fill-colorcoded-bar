303e0b89f8770aa9a21248211957c7316371863b
Julian Gruber <julian@juliangruber.com>
2015-10-13 10:43:51 +0200

    use partition-refinement-iterator. closes #3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var parallel = require('run-parallel');
 var EventEmitter = require('events').EventEmitter;
+var refinementIterator = require('partition-refinement-iterator');
 
 module.exports = function(bar, opts, fetch){
   if (typeof opts == 'function') {
@@ -52,30 +53,7 @@ module.exports = function(bar, opts, fetch){
 var iterators = {};
 
 iterators.refine = function(length){
-  if (typeof length == 'undefined') {
-    throw new Error('refine strategy requires .length');
-  }
-  var next = [];
-  var emitted = [];
-  var divisor = 2;
-  var done = false;
-
-  return function(){
-    if (!next.length && !done) {
-      var seg = length / divisor;
-      for (var i = 0; i < divisor; i++) {
-        var val = Math.floor(seg * i);
-        if (emitted.indexOf(val) > -1) continue;
-        next.push(val);
-        emitted.push(val);
-      }
-      if (divisor >= length && seg < 1) done = true;
-      else divisor *= 2;
-    }
-
-    var i = next.shift();
-    return i;
-  };
+  return refinementIterator(length);
 };
 
 iterators.topdown = function(length){
